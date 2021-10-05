76583485471fecbee58373ec6bd36bcee479472c
divrk <botkavishka@gmail.com>
2021-10-05 21:23:37 +0530

    process.exit

--- bot/config.js
+++ bot/config.js
@@ -1,13 +1,14 @@
 const logger = require('./helpers/logger')
 
 const config = {
-  bot: process.env.BOT_TOKEN ? process.env.BOT_TOKEN : logger.error(`BOT_TOKEN undefined!`) ,
+  bot: process.env.BOT_TOKEN ? process.env.BOT_TOKEN : logger.error(`BOT_TOKEN undefined!`),
   genius: process.env.GENIUS ? process.env.GENIUS : logger.error(`GENIUS TOKEN undefined!`) ,
   channel: process.env.CHANNEL_ID ? process.env.CHANNEL_ID : logger.error(`CHANNEL_ID undefined`)
 }
 
-if (config.bot && config.genius && config.channel === ''){
+if (config.bot && config.genius && config.channel === undefined){
   logger.error('Some Environmental variables are empty')
+  process.exit(1)
 }
 
 module.exports = config
