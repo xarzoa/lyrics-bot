f3e737d2c2342c4721ca3584df8b23c23904dcb0
divrk <botkavishka@gmail.com>
2021-10-06 09:21:27 +0530

    path

--- bot/helpers/web.js
+++ bot/helpers/web.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const path = require('path')
 const app = express()
 
 const web = (port) => {
-  app.set('views' , path.join( './', "views"))
+  app.set('views')
   app.set('view engine' ,'ejs')
   app.get('/', (req, res) => {
   res.render('index')
