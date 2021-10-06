9266c1710cfa2f9f969e41d0c4e06257a40c2d5f
divrk <botkavishka@gmail.com>
2021-10-06 09:17:01 +0530

    useless

--- bot/helpers/web.js
+++ bot/helpers/web.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const web = (port) => {
-  app.set('views' , './')
+  app.set('views' , path.join( './', "views"))
   app.set('view engine' ,'ejs')
   app.get('/', (req, res) => {
   res.render('index')
