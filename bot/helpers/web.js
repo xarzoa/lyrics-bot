010d489f51883c365a745b0388b1f95e47448a47
d4rk <78039335+divrk@users.noreply.github.com>
2021-10-06 04:02:45 +0000

    web.js

--- bot/helpers/web.js
+++ bot/helpers/web.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const viewPath = path.join(__dirname, './views');
+
+app.set('views', viewPath);
+app.set('view engine' ,'ejs')
 
 const web = (port) => {
-  app.set('views')
-  app.set('view engine' ,'ejs')
   app.get('/', (req, res) => {
   res.render('index')
   })
