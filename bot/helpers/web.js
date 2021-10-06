bbfa479153de2ebf479e6919c2d9b42f1e0026ad
divrk <botkavishka@gmail.com>
2021-10-06 08:52:29 +0530

    views

--- bot/helpers/web.js
+++ bot/helpers/web.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 
 const web = (port) => {
+  app.set('views' , './bot/helpers')
   app.set('view engine' ,'ejs')
   app.get('/', (req, res) => {
-  res.render('./bot/helpers/views/index')
+  res.render('index')
   })
     
   app.listen(port)
