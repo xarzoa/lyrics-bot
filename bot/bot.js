ac918a712e75049e6e5157c95c97c9a51191d396
divrk <botkavishka@gmail.com>
2021-10-07 13:51:34 +0530

    hehe

--- bot/bot.js
+++ bot/bot.js
@@ -11,7 +11,7 @@ const channelId = config.channel
 const port = config.port
 const username = config.username
 
-config.web && username ? web.web(port,username) : logger.info(`No website!`)
+config.web ? web.web(port,username) : logger.info(`No website!`)
 
 bot.telegram.setMyCommands([
       {
