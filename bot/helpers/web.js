95abced4dca2e6c601b4d242542125833260c1d6
d4rk <botkavishka@gmail.com>
2021-10-13 06:17:30 +0530

    webpage

--- bot/helpers/web.js
+++ bot/helpers/web.js
@@ -6,11 +6,10 @@ const viewPath = path.join(__dirname, './views')
 app.set('views', viewPath);
 app.set('view engine' ,'ejs')
 
-const web = (port, username) => {
-  app.get('/', (req, res) => {
-  res.render('index',{invite:"https://t.me/" + username+ "?/lyrics%20Dandelions"})
+const web = (port,username) => {
+  app.get('/', (req,res) => {
+  res.render('index',{invite:"https://t.me/" + username})
   })
-  
   app.listen(port)
 }
 
